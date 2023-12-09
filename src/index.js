372c92c8fc196c7ac38a0bf463204bb82542a3a6
annabwaldheim <annabwaldheim@gmail.com>
2023-12-09 02:04:47 -0300

    desafio de classificador de nivel de herói

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ let xpHeroi = 9500
 let nivel = "indefinido"
 if (xpHeroi < 1000){
     nivel = "Ferro"
-} else if (2000 > xpHeroi && xpHeroi > 1000){
+} else if (1001 < xpHeroi && xpHeroi < 2000){
     nivel = "Bronze"
-} else if (5000 > xpHeroi  && xpHeroi > 2001){
+} else if (2001 < xpHeroi && xpHeroi < 5000){
     nivel = "Prata"
-} else if (7000 > xpHeroi && xpHeroi > 6001){
+} else if (6001 < xpHeroi && xpHeroi < 7000){
     nivel = "Ouro"
-} else if (8000 > xpHeroi && xpHeroi > 7001){
+} else if (7001 < xpHeroi && xpHeroi < 8000){
     nivel = "Platina"
-} else if (9000 > xpHeroi && xpHeroi > 8001){
+} else if (8001 < xpHeroi && xpHeroi < 9000){
     nivel = "Ascendente"
-} else if (10000 > xpHeroi && xpHeroi > 9001){
+} else if (9001 < xpHeroi && xpHeroi < 10000){
     nivel = "Imortal"
 } else if (xpHeroi >= 10001){
     nivel = "Radiante"
 }
-console.log (xpHeroi)
+
 console.log("O Herói de nome " + nomeHeroi + " está no nível " + nivel)
